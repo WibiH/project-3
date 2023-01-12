d46c6d96ed73930e107af025eec4f92bfb33a316
Q <morningqwhy@yahoo.com.hk>
2023-01-12 19:41:51 +0000

    update to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,51 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import EventDisplayAll from "./pages/EventDisplayAllPage";
 import HomePage from "./pages/HomePage";
 import Profile from "./pages/ProfilePage";
 import SignUp from "./pages/SignUpPage";
 import LogIn from "./pages/LogInPage";
 import { AuthProviderWrapper } from "./context/authentication";
+// import { useAuthContext } from "./context/authentication";
+import EventCreatePage from "./pages/EventCreatePage";
+import EventEditDeletePage from "./pages/EventEditDeletePage";
+import EventDisplaySinglePage from "./pages/EventEditDeletePage";
+import EventDisplayAllPage from "./pages/EventDisplayAllPage";
 import Tour from "./pages/Tour";
 
 function App() {
+  // const { user } = useAuthContext();
+
   return (
     <div className="App">
       <AuthProviderWrapper>
         <Navbar />
         <Routes>
           <Route path="/home" element={<HomePage />} />
-          <Route path="/events" element={<EventDisplayAll />} />
+          <Route path="/events" element={<EventDisplayAllPage />} />
+          <Route
+            path="/events/create"
+            element={
+              <EventCreatePage />
+              // <EventCreatePage user={user} />
+              // user && <EventCreatePage />
+              // || (
+              //   <AuthenticationRequiredErrorPage />
+              // )
+            }
+          />
+
+          <Route
+            path="/events/:id/edit"
+            element={
+              <EventEditDeletePage />
+              // (user && <EventEditDeletePage />) || (
+              //   <AuthenticationRequiredErrorPage />
+              // )
+            }
+          />
+          <Route path="/events/:id" element={<EventDisplaySinglePage />} />
+
           <Route path="/profile" element={<Profile />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/login" element={<LogIn />} />
